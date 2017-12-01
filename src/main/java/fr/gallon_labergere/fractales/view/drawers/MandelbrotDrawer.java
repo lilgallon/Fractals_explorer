6eea90d5a326bc94e54e5ca0ea554460f75d7a4c
nerodev <lilian.gallon@etu.u-bordeaux.fr>
2017-12-01 13:32:00 +0100

    Removed and added todo stuff in the mandelbrot drawer

--- src/main/java/fr/gallon_labergere/fractales/view/drawers/MandelbrotDrawer.java
+++ src/main/java/fr/gallon_labergere/fractales/view/drawers/MandelbrotDrawer.java
@@ -8,6 +8,12 @@ import java.awt.*;
 
 public class MandelbrotDrawer implements IFractalDrawer {
 
+    /**
+     * TODO:
+     * Retirer les MIN / MAX etc... ?
+     * Pour le moment pas sur, c'est peut-etre de la que vient le probleme de precision.
+     */
+
     private final double MIN_X;
     private final double MAX_X;
     private final double MIN_Y;
@@ -20,68 +26,36 @@ public class MandelbrotDrawer implements IFractalDrawer {
         MAX_Y = maxY;
     }
 
+    /**
+     * Method that contain the fractal calculation algorithm
+     * @param g graphics
+     * @param width view width
+     * @param height view height
+     * @param settingsModel model
+     * @param settingsController controller
+     */
     @Override
     public void draw(Graphics g, int width, int height, Settings settingsModel, SettingsController settingsController) {
 
-//        float mapRangeX = MAX_X - MIN_X;
-//        float mapRangeY = MAX_Y - MIN_Y;
-//
-//        System.out.println("Drawing from (" + settings.getMapX(0) + " ; " + settings.getMapY(0) + ") to ("
-//            + settings.getMapX(width) + " ; " + settings.getMapY(height) + ")");
-//        int vxmin = settings.getMapX(0);
-//        int vymax = settings.getMapY(height);
-//
-//        // Pour chaque coordonnée Y, faire :
-//        for (int y = 0; y < height; y++) {
-//            int vy = settings.getMapY(y);
-//            int b = vymax - vy;
-//
-//            // Pour chaque coordonnée X, faire :
-//            for (int x = 0; x < width; x++) {
-//                int vx = settings.getMapX(x);
-//                int a = vx + vxmin; // a = c_i
-//
-//                int tx = 0, ty = 0, n = 0;
-//                do {
-//                    int temp = tx * tx - ty * ty + a; // temp = z_r (partie réelle)
-//                    ty = 2 * tx * ty + b; // ty = z_i (partie imaginaire)
-//                    tx = temp;
-//                    n++;
-//                } while (tx * tx + ty * ty < 4 && n <= 100);
-//                g.setColor(n == 101 ? Color.BLACK : new Color((n / 100f), 0f, 0f));
-//                g.fillRect(x, y, 1, 1);
-//            }
-//        }
-
-
-        System.out.println("calcul" + settingsModel.getCenterX() + ";" + settingsModel.getCenterY() );
-         /**
-         * TODO:
-         * 21/11/17
-         *
-         * 1)
-         * On ne peut pas se déplacer dans la fractale, on peut zoomer, mais pas sur un point
-         * précis, on reste sur (0,0).
-         * Quand on se déplace, c'est en fait le rendu qui ne se fait que sur une certaine zone.
-         * Je m'explique, quand on veut se déplacer avec le curseur, la zone dans laquelle sera
-         * dessinée la fractale est déplacée, MAIS pas la fractale. C'est-à-dire qu'à force de
-         * se déplacer, la fractale sera coupée.
-         * Il faut trouver le truc qui fait qu'on peut déplacer la fractale (ça doit etre un truc
-         * à la con enplus)
-          *
-          * update 28/11/17:
-          * En fait il ne faut pas confondre les coordonnées de la fractale, et les coordonnées
-          * où est-ce qu'on va les dessiner. Le problème se pose pas quand on ne zoom pas. Mais dès qu'on zoom
-          * ces coordonnées ne sont pas liées.
-         *
-         * 2)
+        /**
+          * TODO:
          * Pour le lol on pourrait changer la couleur de la fractale bleu / vert / rouge, et
          * d'autres nuances (ça fait pro)
+         */
+
+        /**
+         * TODO:
+         * Grosse opti à faire : avoir un faible it_max au début, pour ensuite le faire augmenter avec le zoom.
+         * En effet, pas besoin d'être très précis quand on a pas zoom, on augmentera ainsi la précision en zoomant.
          */
 
         // Maximum number of iteration before stopping the calculation by supposing that the suite is convergent.
         double it_max = 50;
 
+        /**
+         * TODO:
+         * Améliorer le zoom (ça vaudrait mieux un zoom *= 2) parce que là ça n'a pas tellement de sens
+         */
         // The default zoom is not correct for mandelbrot, here are the adjustments
         double zoom = settingsModel.getZoomLevel()*200+100;
 
