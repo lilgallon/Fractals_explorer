8a61422e088f32d4c658c91c02cb86b1ceaa0752
Lilian <lilian.gallon@etu.u-bordeaux.fr>
2017-12-06 19:53:24 +0100

    Tryied to make the newton fractal

---
+++ src/main/java/fr/gallon_labergere/fractales/utils/Complex.java
@@ -0,0 +1,130 @@
+package fr.gallon_labergere.fractales.utils;
+
+/**
+ * Created by lgallon on 06/12/17.
+ */
+public class Complex {
+
+    private double real;
+    private double imag;
+
+    public Complex(double real, double imag){
+        this.real = real;
+        this.imag = imag;
+    }
+
+    /**
+     * this + z
+     * @param z
+     * @return
+     */
+    public Complex plus(Complex z){
+        double new_real = real + z.getReal();
+        double new_imag = imag + z.getImag();
+        return new Complex(new_real,new_imag);
+    }
+
+    /**
+     * this + a
+     * @param a
+     * @return
+     */
+    public Complex plus(double a){
+        double new_real = real + a;
+        double new_imag = imag + a;
+        return new Complex(new_real,new_imag);
+    }
+
+    /**
+     * this - z
+     * @param z
+     * @return
+     */
+    public Complex minus(Complex z){
+        double new_real = real - z.getReal();
+        double new_imag = imag - z.getImag();
+        return new Complex(new_real,new_imag);
+    }
+
+    /**
+     * this - a
+     * @param a
+     * @return
+     */
+    public Complex minus(double a){
+        double new_real = real - a;
+        double new_imag = imag - a;
+        return new Complex(new_real,new_imag);
+    }
+
+    /**
+     * this * z
+     * @param z
+     * @return
+     */
+    public Complex times(Complex z){
+        double new_real = real * z.getReal() - imag * z.getImag();
+        double new_imag = real * z.getImag() + imag * z.getReal();
+        return new Complex(new_real, new_imag);
+    }
+
+    /**
+     * this * a
+     * @return a
+     */
+    public Complex scale(double a) {
+        return new Complex(real * a, imag * a);
+    }
+
+    /**
+     * this / z
+     * @param z
+     * @return
+     */
+    public Complex divides(Complex z) {
+        return z.times(z.reciprocal());
+    }
+
+    /**
+     *
+     * @return the reciprocal
+     */
+    public Complex reciprocal() {
+        double scale = real*real + imag*imag;
+        return new Complex(real / scale, -imag / scale);
+    }
+
+    /**
+     * module(this) => |this|
+     * @return
+     */
+    public double getModule(){
+        return Math.sqrt(real*real+imag*imag);
+    }
+
+    /**
+     * this == z
+     * @param o
+     * @return
+     */
+    public boolean equals(Object o){
+        if(o==null || !(o instanceof Complex)) return false;
+        return ((Complex) o).getReal()==real && ((Complex) o).getImag()==imag;
+    }
+
+    public double getReal() {
+        return real;
+    }
+
+    public void setReal(double real) {
+        this.real = real;
+    }
+
+    public double getImag() {
+        return imag;
+    }
+
+    public void setImag(double imag) {
+        this.imag = imag;
+    }
+}
--- src/main/java/fr/gallon_labergere/fractales/view/drawers/OtherDrawer.java
+++ src/main/java/fr/gallon_labergere/fractales/view/drawers/OtherDrawer.java
@@ -2,8 +2,10 @@ package fr.gallon_labergere.fractales.view.drawers;
 
 import fr.gallon_labergere.fractales.controller.SettingsController;
 import fr.gallon_labergere.fractales.model.Settings;
+import fr.gallon_labergere.fractales.utils.Complex;
 
 import java.awt.*;
+
 
 public class OtherDrawer implements IFractalDrawer {
 
@@ -14,8 +16,8 @@ public class OtherDrawer implements IFractalDrawer {
 
     private final float INITIAL_ZOOM = 1;
     private final float MIN_ZOOM = 1f;
-    private final float MAX_ZOOM = 2f;
-    private final float ZOOM_MULTIPLICATOR = 1.5f;
+    private final float MAX_ZOOM = 1000f;
+    private final float ZOOM_MULTIPLICATOR = 1.2f;
 
     private final int INITIAL_ITERATIONS = 25;
     private final int MIN_ITERATIONS = 25;
@@ -28,6 +30,22 @@ public class OtherDrawer implements IFractalDrawer {
         MAX_Y = maxY;
     }
 
+
+    // newton fractal https://en.wikipedia.org/wiki/Newton_fractal
+    private Complex fractal_function(Complex z){
+        // f(z) = z * z * z - 1.0 => z^3 - 1.0
+        return (z.times(z).times(z)).minus(-1.0);
+    }
+
+    private double module(Complex z){
+        return Math.sqrt(z.getReal()*z.getReal()+z.getImag()*z.getImag());
+    }
+
+    private Complex fractal_function_d(Complex z){
+        // f'(z) = z * z * 2 => z^2 * 3
+        return (z.times(z)).scale(3);
+    }
+
     @Override
     public void draw(Graphics g, int width, int height, Settings settingsModel, SettingsController settingsController) {
         g.setColor(Color.WHITE);
@@ -35,6 +53,40 @@ public class OtherDrawer implements IFractalDrawer {
         g.drawLine(-1000, settingsModel.getCenterY(), 1000, settingsModel.getCenterY());
         System.out.println("Drawing from (" + settingsModel.getMapX(0) + " ; " + settingsModel.getMapX(width) + ") and ("
                 + settingsModel.getMapY(0) + " ; " + settingsModel.getMapY(height) + ")");
+
+        float tolerance = 0.0001f;
+
+        for (int x=0 ; x<width ; ++x){
+            for(int y=0; y<height ; ++y){
+                double z_i = (y - settingsModel.getCenterY())/settingsModel.getZoomLevel();//(y * (yb - ya ) / (height -1) + ya ) ;//+ settingsModel.getCenterY() /settingsModel.getZoomLevel();
+                double z_r = (x - settingsModel.getCenterX())/settingsModel.getZoomLevel();//(x * (xb - xa) / (height -1) + xa ) ;//+ settingsModel.getCenterX() /settingsModel.getZoomLevel();
+
+                Complex z = new Complex(z_r , z_i);
+
+                int it = 0;
+                Complex z1 = new Complex(-1000,-1000);
+
+                do{
+                    // dz => f'(z)
+                    Complex dz = fractal_function_d(z);
+                    // zn+1 = zn - f(zn)/f'(zn)
+                    z1 = z.minus( fractal_function(z).divides(dz) );
+
+                    // zn = zn+1
+                    z = z1;
+
+                    ++it;
+                }while (module(fractal_function(z))>tolerance && it < settingsModel.getIterations());
+
+                g.setColor(new Color(it%4*64, it%8*32,it%16*16));
+
+                // z -> oo
+                if(it==MAX_ITERATIONS) g.setColor(Color.red);
+
+                g.fillRect(x,y,1,1);
+            }
+        }
+
     }
 
     @Override
